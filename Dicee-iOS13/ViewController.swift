d558644cad77d199065387c8290db972fe101152
Konstantin <don.kamatoz@bk.ru>
2023-01-20 23:23:12 +0500

    Третий урок четвертого модуля. Добавил взаимодействие с кнопкой - по нажатию кнопку меняет изображение у других элементов.

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -12,16 +12,25 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var diceImageViewOne: UIImageView!
     @IBOutlet weak var diceImageViewTwo: UIImageView!
-
+    @IBOutlet weak var diceButtonRoll: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    @IBAction func rollButtonTouchedDown(_ sender: UIButton) {
+        diceButtonRoll.alpha = 0.5
+    }
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
         
+        diceButtonRoll.alpha = 1.0
+
         diceImageViewOne.image = UIImage(imageLiteralResourceName: "DiceSix")
 //        diceeImageViewOne.alpha = 0.5
         diceImageViewTwo.image = UIImage(imageLiteralResourceName: "DiceTwo")
 
     }
-
 
 }
 
